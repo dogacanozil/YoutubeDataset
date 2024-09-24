9c6ff5e955ba44764bf83ac8c08d59fe792836ea
Can Ozil <ozil@soleynet.soley.io>
2024-09-24 16:21:38 +0200

    Update

--- src/utils/video_utils.py
+++ src/utils/video_utils.py
@@ -5,64 +5,96 @@ import gzip
 import csv
 from datetime import datetime
 from utils.string_utils import convert_time_to_seconds, convert_views_to_int
+import json
 
-def add_videos_to_list_per_channel_id(channel_id, limit, sort_by, video_set, video_list):
+def add_channel_video_list_per_channel_id_to_list(channel_id, threshold):
+    channel_video_list = []
+    video_count = 0
+    video_Set = set()
+    for video in scrapetube.get_channel(channel_id, limit=threshold):
+        video_count=video_count+1
+    if video_count == threshold:
+        channel_video_list.extend(add_videos_to_list_per_channel_id_per_type(channel_id, threshold//3, 'popular', video_Set))
+        channel_video_list.extend(add_videos_to_list_per_channel_id_per_type(channel_id, threshold//3, 'newest', video_Set))
+        channel_video_list.extend(add_videos_to_list_per_channel_id_per_type(channel_id, threshold//3, 'oldest', video_Set))
+    else:
+        channel_video_list.extend(add_videos_to_list_per_channel_id_per_type(channel_id, threshold, 'newest', video_Set))
+    return channel_video_list
 
+def add_videos_to_list_per_channel_id_per_type(channel_id, limit, sort_by, video_set):
+    video_list = []
     videos = scrapetube.get_channel(channel_id, limit=limit, sort_by=sort_by)
     for video in videos:
         current_time = datetime.now()
-        if 'thumbnailOverlayTimeStatusRenderer' in video["thumbnailOverlays"][0]:
-            if video['videoId'] not in video_set and video["thumbnailOverlays"][0]["thumbnailOverlayTimeStatusRenderer"]["style"]!='LIVE':
-                
-                if "descriptionSnippet" in video:
-                    video_description = video["descriptionSnippet"]["runs"][0]["text"].replace("\n", "")
-                else:
-                    video_description = ""
+        if 'thumbnailOverlays' in video:
+            if 'thumbnailOverlayTimeStatusRenderer' in video["thumbnailOverlays"][0]:
+                if video['videoId'] not in video_set and video["thumbnailOverlays"][0]["thumbnailOverlayTimeStatusRenderer"]["style"]!='LIVE':
                     
-                if "upcomingEventData" in video:
-                    view_count = None
-                    publish_date = None
-                elif 'viewCountText' not in video:
-                    view_count = None
-                    publish_date = video['publishedTimeText']['simpleText']
-                elif video['viewCountText']['simpleText']=="No views":
-                    view_count = 0
-                    publish_date = video['publishedTimeText']['simpleText']
-                else:
-                    view_count = convert_views_to_int(video['viewCountText']['simpleText'])
-                    publish_date = video['publishedTimeText']['simpleText']
-                    
-                if 'title' in video:
-                    if 'runs' in video['title']:
-                        video_title = video['title']['runs'][0]['text']
-                    elif 'simpleText' in video['title']:
-                        video_title = video['title']['simpleText']
-                else:
-                    video_title = None
-                    
-                video_list.append([channel_id, video['videoId'], video_title, convert_time_to_seconds(video['lengthText']['simpleText']), video_description, video['thumbnail']['thumbnails'][0]['url'], view_count, publish_date, current_time])
-                video_set.add(video['videoId'])
+                    if "descriptionSnippet" in video:
+                        video_description = video["descriptionSnippet"]["runs"][0]["text"].replace("\n", "")
+                    else:
+                        video_description = ""
+                        
+                    if "upcomingEventData" in video:
+                        view_count = None
+                        publish_date = None
+                    elif 'viewCountText' not in video:
+                        view_count = None
+                        publish_date = video['publishedTimeText']['simpleText']
+                    elif video['viewCountText']['simpleText']=="No views":
+                        view_count = 0
+                        publish_date = video['publishedTimeText']['simpleText']
+                    else:
+                        view_count = convert_views_to_int(video['viewCountText']['simpleText'])
+                        publish_date = video['publishedTimeText']['simpleText']
+                        
+                    if 'title' in video:
+                        if 'runs' in video['title']:
+                            video_title = video['title']['runs'][0]['text']
+                        elif 'simpleText' in video['title']:
+                            video_title = video['title']['simpleText']
+                    else:
+                        video_title = None
+
+                    if 'lengthText' in video:
+                        video_length = convert_time_to_seconds(video['lengthText']['simpleText'])
+                    else:
+                        video_length = None
+                        
+                    video_list.append([channel_id, video['videoId'], video_title, video_length, video_description, video['thumbnail']['thumbnails'][0]['url'], view_count, publish_date, current_time])
+                    video_set.add(video['videoId'])
+    return video_list
 
 
 
-def create_channel_video_relationship_list_limited_videos(channel_ids_df:pd.DataFrame, threshold:int = 60):
+def create_channel_video_relationship_list_limited_videos(channel_ids_df:pd.DataFrame, threshold:int = 60, counter:int = 0):
 
     channel_video_list =[]
     channel_index = 0
-    for row in channel_ids_df.itertuples():
-        print(channel_index)
+    
+    for row in tqdm(channel_ids_df.itertuples()):
         channel_index = channel_index+1
         if row.is_active is True:
-            video_count = 0
-            channel_set = set()
-            for video in scrapetube.get_channel(row.channel_id, limit=threshold):
-                video_count=video_count+1
-            if video_count == threshold:
-                add_videos_to_list_per_channel_id(row.channel_id, threshold//3, 'popular', channel_set, channel_video_list)
-                add_videos_to_list_per_channel_id(row.channel_id, threshold//3, 'newest', channel_set, channel_video_list)
-                add_videos_to_list_per_channel_id(row.channel_id, threshold//3, 'oldest', channel_set, channel_video_list)
-            else:
-                add_videos_to_list_per_channel_id(row.channel_id, threshold, 'newest', channel_set, channel_video_list)
+            channel_video_list.extend(add_channel_video_list_per_channel_id_to_list(row.channel_id, threshold))
+            # except json.decoder.JSONDecodeError as e:
+            #     if counter<10:
+            #         print(f"JSON Decode Error: {e}, channel_id: {row.channel_id}")
+            #         print(f"Retry for the {counter} time")
+            #         return create_channel_video_relationship_list_limited_videos(channel_ids_df, threshold, counter+1)
+            #     else:
+            #         print(f"Raise error after 10 trial")
+            #         print(f"JSON Decode Error: {e}, channel_id: {row.channel_id}")
+            #         raise
+
+            # except TypeError as e:
+            #     if counter<10:
+            #         print(f"Type Error: {e}, channel_id: {row.channel_id}")
+            #         print(f"Retry for the {counter} time")
+            #         return create_channel_video_relationship_list_limited_videos(channel_ids_df, threshold, counter+1)
+            #     else:
+            #         print(f"Raise error after 10 trial")
+            #         print(f"Type Error: {e}, channel_id: {row.channel_id}")
+            #         raise
 
     return channel_video_list
 
